968e4802f252813d492b4e0220494c1f12605596
hanshenrik <hh.gronsleth@gmail.com>
2016-02-03 19:58:02 +0100

    Make social buttons fade in sequentially

--- js/script.js
+++ js/script.js
@@ -1,7 +1,25 @@
+var animationDuration = 2222;
+var delay = 666;
+
+jQuery.fn.extend({
+  fadeInAndSlideDown: function () {
+    $( this )
+      // .css( { 'margin-top': '-300px', opacity: 0 } )
+      .animate( { opacity: 1 }, animationDuration );
+  }
+});
+
 $(document).ready( function() {
   $( '#current-year' ).text( (new Date).getFullYear() );
 });
 
 $(window).on( 'load' , function() {
   $( 'body' ).fadeIn(1000);
+
+  $( '.social-item' ).each( function(i) {
+    var item = $(this);
+    setTimeout( function () {
+       item.fadeInAndSlideDown()
+   }, i * delay);
+  });
 });
